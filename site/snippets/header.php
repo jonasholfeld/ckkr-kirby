2d1ca9b86e310e5d1d6f41d1564890eac0e0dd3f
Jonas Holfeld <klatschpauke@googlemail.com>
2023-04-21 11:20:30 +0200

    added scss files

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,5 +8,4 @@
 
     <?php snippet('scss'); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="/node_modules/@sindresorhus/slugify/index.js" type="module"></script>
 </head>
